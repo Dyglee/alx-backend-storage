50adf32e30fe790733ffd2ee9f409290f165e201
Yassine <yassinemoussafir31@gmail.com>
2024-08-08 09:19:41 +0000

    1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- 1-country_users.sql
+-- Create users table with id, email, name, and country
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
